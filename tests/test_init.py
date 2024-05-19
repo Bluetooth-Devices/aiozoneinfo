4572469da8dfd1d1e9f0145edc43198017bc8c1b
J. Nick Koston <nick@koston.org>
2024-05-18 22:12:57 -1000

    fix: python min 3.9

--- tests/test_init.py
+++ tests/test_init.py
@@ -7,6 +7,6 @@ from aiozoneinfo import async_get_time_zone
 @pytest.mark.asyncio
 async def test_async_get_time_zone():
     """Getting cached timezone."""
-    zone_info = await async_get_time_zone("America/New_York")
+    zone_info = await async_get_time_zone("America/Los_Angeles")
     assert isinstance(zone_info, zoneinfo.ZoneInfo)
-    assert zone_info.key == "America/New_York"
+    assert zone_info.key == "America/Los_Angeles"
